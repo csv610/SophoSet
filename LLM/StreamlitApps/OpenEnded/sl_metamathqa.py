683e5e162caefee0091bf9156b8236202377742e
Chaman Singh Verma <csv610@gmail.com>
2024-09-11 17:34:47 +0530

    llm added

--- LLM/StreamlitApps/OpenEnded/sl_metamathqa.py
+++ LLM/StreamlitApps/OpenEnded/sl_metamathqa.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datasets import load_dataset
+from llm_chat import LLMChat
 
 # Load the dataset
 @st.cache_data()
@@ -12,6 +13,37 @@ def load_data(split='train'):
     except Exception as e:
         st.error(f"Error loading dataset: {str(e)}")
         return None
+    
+@st.cache_resource
+def load_llm_model(model_name: str):
+    """Initialize and cache the LLM."""
+    return LLMChat(model_name)
+
+def talk_to_llm(i, row, model_name):
+    question = row['query']
+
+    llm = load_llm_model(model_name)  # Changed from upload_llm_model to load_llm_model
+
+    if st.button(f"LLM Answer:{i+1}"):
+        with st.spinner("Processing ..."):
+
+            response = llm.get_answer(question)
+            st.write(f"Answer: {response['answer']}")
+            st.write(f"Number of Input words: {response['num_input_words']}")
+            st.write(f"Number of output  words: {response['num_output_words']}")
+            st.write(f"Time: {response['response_time']}")
+
+    if st.button(f"Ask Question: {i+1}"):
+        user_question = st.text_area(f"Edit Question {i+1}", height=100)
+        if user_question is not None:
+              print( "USER QUESTION; ", user_question)
+              with st.spinner("Processing ..."):
+                   prompt = "Based on the Context of " + question + " Answer the User Question: " + user_question
+                   response = llm.get_answer(prompt)
+                   st.write(f"Answer: {response['answer']}")
+                   st.write(f"Number of Input words: {response['num_input_words']}")
+                   st.write(f"Number of output  words: {response['num_output_words']}")
+                   st.write(f"Time: {response['response_time']}")
 
 # Streamlit app
 def main():
@@ -41,11 +73,16 @@ def main():
     start_index = (selected_page - 1) * num_items_per_page
     end_index = min(start_index + num_items_per_page, total_items)
 
+    model_name = "llama3.1"
     for i in range(start_index, end_index):
         row = dataset[i]
         st.header(f"Question {i + 1}")
         st.write(row['query'])
-        st.write(f"Answer: {row['response']}")
+
+        if st.button(f"Human Answer :{i+1}"):
+           st.write(f"Answer: {row['response']}")
+
+        talk_to_llm(i+1, row, model_name)
 
         st.divider()  # Divider between items
 

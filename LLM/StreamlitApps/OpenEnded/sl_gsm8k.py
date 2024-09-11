6686eee1891e4d19a166238c687b72cf198b23e8
Chaman Singh Verma <csv610@gmail.com>
2024-09-11 13:47:33 +0530

    llm added

--- LLM/StreamlitApps/OpenEnded/sl_gsm8k.py
+++ LLM/StreamlitApps/OpenEnded/sl_gsm8k.py
@@ -1,6 +1,8 @@
-import streamlit as st
-from datasets import load_dataset
 import re
+import streamlit as st
+
+from datasets import load_dataset
+from llm_chat import LLMChat
 
 st.set_page_config(layout="wide")
 
@@ -14,6 +16,11 @@ def load_data(subset, split):
     except Exception as e:
         st.error(f"Error loading dataset: {str(e)}")
         return None
+    
+@st.cache_resource
+def load_llm_model(model_name: str = "llama3.1"):
+    """Initialize and cache the LLM."""
+    return LLMChat(model_name)
 
 def rewrite_sentence(text):
     # First, identify and escape LaTeX dollar signs
@@ -61,6 +68,34 @@ def config_panel():
 
     return dataset, num_items_per_page, selected_page
 
+def talk_to_llm(i, row, model_name):
+    question = row['question']
+
+    llm = load_llm_model(model_name)  # Changed from upload_llm_model to load_llm_model
+
+    if st.button(f"LLM Answer:{i+1}"):
+        with st.spinner("Processing ..."):
+
+            response = llm.get_answer(question)
+            st.write(f"Answer: {response['answer']}")
+            st.write(f"Number of Input words: {response['num_input_words']}")
+            st.write(f"Number of output  words: {response['num_output_words']}")
+            st.write(f"Time: {response['response_time']}")
+
+    if st.button(f"Ask Question: {i+1}"):
+        user_question = st.text_area(f"Edit Question {i+1}", height=100)
+        if user_question is not None:
+              print( "USER QUESTION; ", user_question)
+              with st.spinner("Processing ..."):
+                   prompt = "Based on the Context of " + question + " Answer the User Question: " + user_question
+                   response = llm.get_answer(prompt)
+                   st.write(f"Answer: {response['answer']}")
+                   st.write(f"Number of Input words: {response['num_input_words']}")
+                   st.write(f"Number of output  words: {response['num_output_words']}")
+                   st.write(f"Time: {response['response_time']}")
+
+
+
 # Streamlit app
 def view_dataset():
     st.title("Dataset: GSM8K")
@@ -76,6 +111,8 @@ def view_dataset():
     # Display items for the selected page
     start_index = (selected_page - 1) * num_items_per_page
     end_index = min(start_index + num_items_per_page, len(dataset))
+
+    model_name = "llama3.1"
 
     for i in range(start_index, end_index):
         row = dataset[i]
@@ -98,6 +135,8 @@ def view_dataset():
         with col2:
             if st.button(f"Show Solution {i}"):
                 st.write(f"Solution: {solution}")
+
+        talk_to_llm(i, row, model_name)
 
         st.divider()  # Divider between items
 

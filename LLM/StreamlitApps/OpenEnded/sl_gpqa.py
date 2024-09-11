c40e79d2bea9ee576b7ec9ca012202fe2a1b6527
Chaman Singh Verma <csv610@gmail.com>
2024-09-11 13:39:20 +0530

    llm added

--- LLM/StreamlitApps/OpenEnded/sl_gpqa.py
+++ LLM/StreamlitApps/OpenEnded/sl_gpqa.py
@@ -1,6 +1,8 @@
-import streamlit as st
-from datasets import load_dataset
 import re
+import streamlit as st
+
+from datasets import load_dataset
+from llm_chat import LLMChat
 
 hf_token = "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"
 
@@ -16,8 +18,14 @@ def load_data(subset, split):
     except Exception as e:
         st.error(f"Error loading dataset: {str(e)}")
         return None
+    
+@st.cache_resource
+def load_llm_model(model_name: str = "llama3.1"):
+    """Initialize and cache the LLM."""
+    return LLMChat(model_name)
 
 def rewrite_sentence(text):
+    
     # First, identify and escape LaTeX dollar signs
     text = re.sub(r'(?<!\\)\$(.*?)(?<!\\)\$', r'\\$\1\\$', text)
 
@@ -25,6 +33,34 @@ def rewrite_sentence(text):
     text = re.sub(r'\\\$(\d+(\.\d{1,2})?)\\\$', r'$\1$', text)
 
     return text
+
+def talk_to_llm(i, row, model_name):
+    question = row['Pre-Revision Question']
+
+    llm = load_llm_model(model_name)  # Changed from upload_llm_model to load_llm_model
+
+    if st.button(f"LLM Answer:{i+1}"):
+        with st.spinner("Processing ..."):
+
+            response = llm.get_answer(question)
+            st.write(f"Answer: {response['answer']}")
+            st.write(f"Number of Input words: {response['num_input_words']}")
+            st.write(f"Number of output  words: {response['num_output_words']}")
+            st.write(f"Time: {response['response_time']}")
+
+    if st.button(f"Ask Question: {i+1}"):
+        user_question = st.text_area(f"Edit Question {i+1}", height=100)
+        if user_question is not None:
+              print( "USER QUESTION; ", user_question)
+              with st.spinner("Processing ..."):
+                   prompt = "Based on the Context of " + question + " Answer the User Question: " + user_question
+                   response = llm.get_answer(prompt)
+                   st.write(f"Answer: {response['answer']}")
+                   st.write(f"Number of Input words: {response['num_input_words']}")
+                   st.write(f"Number of output  words: {response['num_output_words']}")
+                   st.write(f"Time: {response['response_time']}")
+
+
 
 # Streamlit app
 def main():
@@ -58,6 +94,8 @@ def main():
     start_index = (selected_page - 1) * num_items_per_page
     end_index = min(start_index + num_items_per_page, total_items)
 
+    model_name = "llama3.1"
+    
     for i in range(start_index, end_index):
         row = dataset[i]
         st.header(f"Question: {i + 1}")
@@ -67,8 +105,11 @@ def main():
         st.write(question)
         st.write(" ")
 
-        answer   = row['Pre-Revision Correct Answer']
-        st.write(f"Answer: {answer}")
+        if st.button(f"Human Answer:{i+1}"):
+            answer   = row['Pre-Revision Correct Answer']        
+            st.write(f"Answer: {answer}")
+
+        talk_to_llm(i, row, model_name)
 
         st.divider()
 

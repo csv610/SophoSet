bc64758b1af8b36556c4345dd072c4c3d1e88549
Chaman Singh Verma <csv610@gmail.com>
2024-09-11 14:36:44 +0530

    add llm

--- LLM/StreamlitApps/OpenEnded/sl_imo_geometry.py
+++ LLM/StreamlitApps/OpenEnded/sl_imo_geometry.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+from llm_chat import LLMChat
 from datasets import load_dataset
 
 st.set_page_config(layout="wide")
@@ -12,6 +14,38 @@ def load_data():
     except Exception as e:
         st.error(f"Error loading dataset: {str(e)}")
         return None
+    
+@st.cache_resource
+def load_llm_model(model_name: str = "llama3.1"):
+    """Initialize and cache the LLM."""
+    return LLMChat(model_name)
+
+def talk_to_llm(i, row, model_name):
+    question = row['question']
+
+    llm = load_llm_model(model_name)  # Changed from upload_llm_model to load_llm_model
+
+    if st.button(f"LLM Answer:{i+1}"):
+        with st.spinner("Processing ..."):
+
+            response = llm.get_answer(question)
+            st.write(f"Answer: {response['answer']}")
+            st.write(f"Number of Input words: {response['num_input_words']}")
+            st.write(f"Number of output  words: {response['num_output_words']}")
+            st.write(f"Time: {response['response_time']}")
+
+    if st.button(f"Ask Question: {i+1}"):
+        user_question = st.text_area(f"Edit Question {i+1}", height=100)
+        if user_question is not None:
+              print( "USER QUESTION; ", user_question)
+              with st.spinner("Processing ..."):
+                   prompt = "Based on the Context of " + question + " Answer the User Question: " + user_question
+                   response = llm.get_answer(prompt)
+                   st.write(f"Answer: {response['answer']}")
+                   st.write(f"Number of Input words: {response['num_input_words']}")
+                   st.write(f"Number of output  words: {response['num_output_words']}")
+                   st.write(f"Time: {response['response_time']}")
+# Streamlit app
 
 def config_panel():
     """Function to handle the contents of the left panel (sidebar)."""
@@ -49,6 +83,7 @@ def view_dataset():
     start_index = (selected_page - 1) * num_items_per_page
     end_index = min(start_index + num_items_per_page, len(dataset))
 
+    model_name = "llama3.1"
     for i in range(start_index, end_index):
         row = dataset[i]
         st.header(f"Question: {i + 1}")
@@ -57,6 +92,7 @@ def view_dataset():
         question = row['question']
         # Use the helper function to display text with custom font size and add spacing
         st.write(question)
+        talk_to_llm(i, row, model_name)
         st.divider()  # Divider between items
 
 if __name__ == "__main__":

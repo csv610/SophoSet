c842995795cf23f602a98e83ee2eb6bd9f912d6c
Chaman Singh Verma <csv610@gmail.com>
2024-09-28 11:20:03 +0530

    prompt changed

--- VLM/StreamlitApps/MCQ/sl_scienceQA.py
+++ VLM/StreamlitApps/MCQ/sl_scienceQA.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from datasets import load_dataset
 
+from vlm_chat import LlavaChat
+from io import BytesIO
+import time 
+
 # Load the dataset
 @st.cache_data()
 def load_data(split = 'train'):
@@ -8,16 +12,52 @@ def load_data(split = 'train'):
     ds = ds[split]
     return ds
 
+def load_vlm_model():
+    vlm = LlavaChat()
+    return vlm
+
+def build_prompt(question, options=None):
+    if not options:  # Check if options are empty
+        prompt = f"You are an expert in science subjects. You are given an open-ended question: '{question}'. Provide a detailed answer."
+    else:
+        prompt = f"You are an expert in science subjects. You are given a question '{question}' with the following options: {options}. Think step by step before answering the question and select the best option that answers the question as correctly as possible."
+    return prompt
+
+def ask_vlm(question, options, image, index):
+    if st.button(f"Ask VLM : {index}"):
+        vlm = load_vlm_model()
+        prompt = build_prompt(question, options)
+
+        # Convert image to bytes if it exists
+        if image:
+            
+            img_byte_arr = BytesIO()
+            image.save(img_byte_arr, format='JPEG')  # Save as JPEG
+            img_byte_arr.seek(0)  # Move to the beginning of the byte stream
+            image = img_byte_arr  # Update image to byte stream
+
+        st.session_state.processing = True  # Set processing state
+        with st.spinner("Retrieving answer..."):
+            start_time = time.time()  # Start the timer
+            try:
+                answer = vlm.get_answer(prompt, image)  # Pass the byte stream
+                elapsed_time = time.time() - start_time  # Calculate elapsed time
+                st.write(f"Model answer: {answer}")
+                st.write(f"Elapsed time: {elapsed_time:.3f} seconds")  # Display elapsed time
+            except Exception as e:
+                st.error(f"Error retrieving answer: {str(e)}")
+            finally:
+                st.session_state.processing = False  # Reset processing state
+
+
 # Streamlit app
 def main():
-    st.title("Dataset: ScienceQA")
-    st.divider()
-
+   
     # Load dataset
     dataset = load_data()
 
     # Sidebar for navigation
-    st.sidebar.title("Navigation")
+    st.sidebar.title("ScienceQA")
 
     # Select number of questions per page
     num_items_per_page = st.sidebar.slider("Select Number of Items per Page", min_value=1, max_value=10, value=5)
@@ -38,15 +78,25 @@ def main():
         row = dataset[i]
 
         st.header(f"Question: {i + 1}")
-        st.write(row['question'])
+        question = row['question']
+        st.write(question)
 
+        image = None
         if row['image'] is not None:
            image = row['image']
            st.image(image, caption=f"Item {start_index + i + 1}", use_column_width=True)
 
-        st.write(row['choices'])
-        
-        st.write(f"Answer: {row['answer']}")
+        options = row['choices']
+
+        # Format and display options
+        for idx, option in enumerate(options):
+            st.write(f"({chr(65 + idx)}) {option}")  # chr(65) is 'A'
+    
+        # Add a button to reveal the answer
+        if st.button(f"Correct Answer: {i + 1}"):
+            st.write(f"Answer: {row['answer']}")
+
+        ask_vlm(question, options, image, i+1)
 
         st.divider() 
 
